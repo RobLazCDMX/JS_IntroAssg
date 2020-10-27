36aae541586fff87e65059955f253c225fb7f80b
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2020-10-27 14:23:27 -0600

    first commit

---
+++ Very Easy/script.js
@@ -0,0 +1,12 @@
+var num1, num2, num3;
+num1 = 5;
+num2 = 25;
+num3 = num2 - num1;
+console.log("The differnce between " + num2 + " and " + num1 + " is " + num3);
+
+// Or
+var num4, num5;
+num4 = 5;
+num5 = 25;
+console.log("The differnce between " + num5 + " and " + num4 + " is 20" );
+
